65ad6c2927ed11840d54393f92fedabbeeca3378
DestinyJoyner <destinyjoyner@pursuit.org>
2024-02-07 00:30:33 -0500

    poke controller

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const cors = require("cors")
 const app = express()
 
 const movieController = require("./controllers/movieController.js")
+const pokeController = require("./controllers/pokeController.js")
+
 app.use(express.json())
 app.use(cors())
 app.use("/movies", movieController)
+app.use("/pokemon", pokeController)
 
 
 
