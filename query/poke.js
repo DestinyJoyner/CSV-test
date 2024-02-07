7f092af23257b2873fe4f810db666c4014108431
DestinyJoyner <destinyjoyner@pursuit.org>
2024-02-07 13:17:25 -0500

    check query

--- query/poke.js
+++ query/poke.js
@@ -3,15 +3,15 @@ const db = require("../db/dbConfig.js")
 
 const getAllPokemon = async (name) => {
     let allPokemon
-    
+    console.log(name)
     try {
         if(name){
             allPokemon = await db.any("SELECT * FROM pokemon WHERE name LIKE $1", `%${name}%`)
         }
         else {
-            allPokemon = await db.any("SELECT * FROM pokemon ")
+            allPokemon = await db.any("SELECT * FROM pokemon")
         }
-         
+         console.log(allPokemon)
         return allPokemon
         
     } catch (error) {
