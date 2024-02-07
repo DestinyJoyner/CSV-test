262cd06179390fec00aa14fd71f86798b8993baa
DestinyJoyner <destinyjoyner@pursuit.org>
2024-02-06 23:49:07 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+
+app.get("/", (req,res) => {
+    res.status(200).send("Welcome to test server")
+})
+
+
+module.exports = app
---
+++ db/dbConfig.js
@@ -0,0 +1,23 @@
+const pgp = require("pg-promise")()
+require("dotenv").config()
+
+const {
+    PG_HOST,
+    PG_PORT,
+    PG_DATABASE,
+    PG_USER,
+    PG_PASSWORD,
+    CONNECTION_STRING
+} = process.env
+
+const cn = {
+    host : PG_HOST,
+    port: PG_PORT,
+    database: PG_DATABASE,
+    user: PG_USER,
+    password: PG_PASSWORD
+}
+
+const db = pgp(cn)
+
+module.exports = db
